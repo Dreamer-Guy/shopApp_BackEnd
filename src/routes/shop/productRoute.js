18ad4363ffed8af51f93beeeee84a2d1f11a61a6
Duc Toan <22120376@student.hcmus.edu.vn>
2025-01-10 01:09:18 +0700

    implement get latest product

--- src/routes/shop/productRoute.js
+++ src/routes/shop/productRoute.js
@@ -1,4 +1,4 @@
-import { searchProducts,getRelatedProducts}
+import { searchProducts,getRelatedProducts,getLatestProducts}
 from "../../controllers/shop-controllers/product-controllers.js/productController.js";
 import express from "express";
 
@@ -6,5 +6,7 @@ const productRouter=express.Router();
 
 productRouter.get("/get",searchProducts);
 productRouter.get("/related/:productId",getRelatedProducts);
+productRouter.get('/latest', getLatestProducts);
+
 
 export default productRouter;
